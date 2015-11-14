ca5e7738d67321eafc09d334feef7df48e7e236d
Greyson Parrelli <greyson.parrelli@gmail.com>
2015-11-14 12:59:12 -0800

    Updated demo app.

--- app/src/main/java/com/greysonparrelli/permisodemo/MainActivity.java
+++ app/src/main/java/com/greysonparrelli/permisodemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.greysonparrelli.permisodemo;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,45 +10,118 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.greysonparrelli.permiso.Permiso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Permiso mPermiso;
+
+
+    // =====================================================================
+    // Overrides
+    // =====================================================================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Instantiate Permiso
+        mPermiso = new Permiso(this);
+
+        // Set click listeners
+        findViewById(R.id.btn_single).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                onSingleClick();
+            }
+        });
+        findViewById(R.id.btn_multiple).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMultipleClick();
+            }
+        });
+        findViewById(R.id.btn_duplicate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDuplicateClick();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mPermiso.onPermissionResultsRetrieved(requestCode, permissions, grantResults);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // =====================================================================
+    // Click Listeners
+    // =====================================================================
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    /**
+     * Request a single permission and display whether or not it was granted or denied.
+     */
+    private void onSingleClick() {
+        mPermiso.requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                if (resultSet.areAllPermissionsGranted()) {
+                    Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * Request multiple permissions and display how many were granted.
+     */
+    private void onMultipleClick() {
+        mPermiso.requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                int numGranted = 0;
+                if (resultSet.isPermissionGranted(Manifest.permission.READ_CONTACTS)) {
+                    numGranted++;
+                }
+                if (resultSet.isPermissionGranted(Manifest.permission.READ_CALENDAR)) {
+                    numGranted++;
+                }
+                Toast.makeText(MainActivity.this, numGranted + "/2 Permissions Granted.", Toast.LENGTH_SHORT).show();
+            }
+        }, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALENDAR);
+    }
+
+    /**
+     * Make two simultaneous requests for the same permission. Only one dialog will pop up, and the results from that
+     * one request will be given to both callbacks.
+     */
+    private void onDuplicateClick() {
+        mPermiso.requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                if (resultSet.areAllPermissionsGranted()) {
+                    Toast.makeText(MainActivity.this, "Permission Granted! (1)", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied. (1)", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, Manifest.permission.CAMERA);
+
+        mPermiso.requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                if (resultSet.areAllPermissionsGranted()) {
+                    Toast.makeText(MainActivity.this, "Permission Granted! (2)", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied. (2)", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, Manifest.permission.CAMERA);
     }
 }
